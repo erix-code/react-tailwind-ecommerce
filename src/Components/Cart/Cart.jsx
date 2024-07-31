98b046428e2d9ac0ba840bd1f9399e10dc359964
gru-erick <ermateze@gmail.com>
2024-07-30 23:24:26 -0400

    - Fixing cart error. - Adding scrollbar to the cart.

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -10,9 +10,10 @@ export default function Cart() {
         console.log("CART PRODUCTS", cartProducts);
     }, cartProducts)
     return (
-        <aside className={"fixed top-20 right-0 md:pt-0 xl:pt-0"}
+        <aside className={"fixed top-20 right-0 md:pt-0 xl:pt-0 "}
                hidden={!isCartOpen}>
-            <div className={"bg-white rounded-lg border shadow-2xl md:w-[520px] md:h-[100vh] w-screen h-screen overflow-y-auto"}>
+            <div
+                className={"bg-white rounded-lg border shadow-2xl md:w-[520px] md:h-[100vh] w-screen h-screen overflow-y-auto"}>
                 <div className={"flex justify-between p-3 border border-b-gray-300"}>
                     <div className={"text-lg"}>Mi orden</div>
                     <button onClick={(event) => setIsCartOpen(!isCartOpen)}>
@@ -21,17 +22,19 @@ export default function Cart() {
                 </div>
                 <div className={"body pt-2"}>
                     {cartProducts.map(((product, index) => (
-                        <OrderCart key={index}  product={product}></OrderCart>
+                            <OrderCart key={index} product={product}></OrderCart>
                         )
                     ))}
                 </div>
-                <div className={"flex justify-center mt-10"}>
-                    <button className={"text-center rounded-xl bg-black text-white p-2 w-48 text-md"}>
+                <div className={"flex justify-center items-center mb-20 pt-5"}>
+                    <button className={"rounded-xl bg-black text-white p-2 w-4/5 text-md"}>
                         Comprar
                     </button>
                 </div>
 
+
             </div>
+
         </aside>
     );
 }
--- src/Components/Cart/OrderCart/OrderCart.jsx
+++ src/Components/Cart/OrderCart/OrderCart.jsx
@@ -4,17 +4,17 @@ import {RiCloseLargeLine} from "react-icons/ri";
 export default function OrderCart({product}) {
     console.log(product);
     return(
-        <div className={"grid grid-cols-10 gap-5 items-center"}>
-            <figure className={"w-24 h-24 col-span-2 px-1"}>
+        <div className={"grid grid-cols-12 gap-5 items-center pr-2"}>
+            <figure className={"w-16  md:w-24 md:h-24 col-span-2 px-1"}>
                 <img className={"w-full h-full rounded-lg object-cover"} src={product.image} alt={product.title}/>
             </figure>
-            <h4 className={"text-lg font-light col-span-3 pl-4"}>
+            <h4 className={"md:text-lg font-light col-span-5  pl-2"}>
                 {product.title}
             </h4>
-            <div className={"col-span-2"}>
-                <input className={"border-2 border-gray-500 border-solid rounded-lg w-14 text-center"} type={"number"} value={product.quantity} />
+            <div className={"col-span-2 md:col-span-2"}>
+                <input className={"border-2 border-gray-500 border-solid rounded-lg w-8 md:w-14 text-center"} type={"number"} value={product.quantity} />
             </div>
-            <span className={"col-span-2"}>${product.totalAmount }</span>
+            <span className={"text-sm md:text-md col-span-2"}>${product.totalAmount }</span>
             <RiCloseLargeLine className={"col-span-1"}></RiCloseLargeLine>
         </div>
     )
